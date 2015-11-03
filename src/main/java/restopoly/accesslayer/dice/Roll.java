21689fe10c75e6e8d3bd20adf82cc7e8ca0b690b
Simon K <Oct4v14n@googlemail.com>
2015-11-03 14:56:22 +0100

    Fix player creation with invalid parameters.

---
+++ src/main/java/restopoly/accesslayer/dice/Roll.java
@@ -0,0 +1,14 @@
+package restopoly.accesslayer.dice;
+
+/**
+ * Created by octavian on 13.10.15.
+ */
+public class Roll {
+    private int number;
+
+    public Roll(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() { return number; }
+}
