2b81e5a73a473f2269415ab6f126a9f9d9db63a8
Simon K <Oct4v14n@googlemail.com>
2015-11-17 13:33:49 +0100

    Add Transactions to money transfer.

--- src/main/java/restopoly/accesslayer/banks/BanksController.java
+++ src/main/java/restopoly/accesslayer/banks/BanksController.java
@@ -41,13 +41,8 @@ import java.util.List;
     }
 
     @RequestMapping(value = "/{gameid}/players/{playerid}", method = RequestMethod.GET)
-    public int getBankAccountSaldo(@PathVariable int gameid, String playerid) {
-        Bank bank = bankList.getBank(gameid);
-        if (bank == null)
-            throw new BankNotFoundException();
-        BankAccount bankAccount = bank.getBankAccount(playerid);
-        if (bankAccount == null)
-            throw new BankAccountNotFoundException();
+    public int getBankAccountSaldo(@PathVariable int gameid, @PathVariable String playerid) {
+        BankAccount bankAccount = getBankAccount(gameid, playerid);
         return bankAccount.getSaldo();
     }
 
@@ -55,13 +50,8 @@ import java.util.List;
     @ResponseStatus(HttpStatus.CREATED)
     public List<Event> createBankTransferTo(@PathVariable int gameid, @PathVariable String to,
         @PathVariable int amount, @RequestBody String reason) {
-        Bank bank = bankList.getBank(gameid);
-        if (bank == null)
-            throw new BankNotFoundException();
-        BankAccount bankAccountTo = bank.getBankAccount(to);
-        if (bankAccountTo == null)
-            throw new BankAccountNotFoundException();
-        bankAccountTo.addSaldo(amount);
+        BankAccount bankAccount = getBankAccount(gameid, to);
+        bankAccount.addSaldo(amount);
         return Collections.emptyList();
     }
 
@@ -69,25 +59,58 @@ import java.util.List;
     @ResponseStatus(HttpStatus.CREATED)
     public List<Event> createBankTransferFrom(@PathVariable int gameid, @PathVariable String from,
         @PathVariable int amount, @RequestBody String reason) {
-        Bank bank = bankList.getBank(gameid);
-        if (bank == null)
-            throw new BankNotFoundException();
-        BankAccount bankAccountFrom = bank.getBankAccount(from);
-        if (bankAccountFrom == null)
-            throw new BankAccountNotFoundException();
-        if (bankAccountFrom.getSaldo() < amount)
+        BankAccount bankAccount = getBankAccount(gameid, from);
+        if (bankAccount.getSaldo() < amount)
             throw new BankInsufficientFundsException();
-        bankAccountFrom.addSaldo(-amount);
+        bankAccount.addSaldo(-amount);
         return Collections.emptyList();
     }
 
     @RequestMapping(value = "/{gameid}/transfer/from/{from}/to/{to}/{amount}", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    public List<Event> createBankTransfer(@PathVariable int gameid, @PathVariable String from,
+    public synchronized List<Event> createBankTransfer(@PathVariable int gameid, @PathVariable String from,
         @PathVariable String to, @PathVariable int amount, @RequestBody String reason) {
-        List<Event> events;
-        events = createBankTransferFrom(gameid, from, amount, reason);
-        events.addAll(createBankTransferTo(gameid, to, amount, reason));
+        List<Event> events = Collections.emptyList();
+
+        // Here we get the values before we change them.
+        BankAccount fromAccount = getBankAccount(gameid, from);
+        BankAccount toAccount = getBankAccount(gameid, to);
+        int fromSaldo = fromAccount.getSaldo();
+        int toSaldo = toAccount.getSaldo();
+
+        try {
+            // Here we will change values.
+            events = createBankTransferFrom(gameid, from, amount, reason);
+
+            // Simulate an Exception after we collected the money from "from",
+            // without giving it to "to".
+            // Also: Mobbing Mary Poppins.
+            if ("Supercalifragilisticexpialigetisch".equals(reason))
+                throw new RuntimeException("Alles ist doof, ich will nicht mehr!");
+
+            events.addAll(createBankTransferTo(gameid, to, amount, reason));
+        } catch (Throwable e) {
+            // If an Exception is thrown while we change values, we will revert the changes.
+            fromAccount.setSaldo(fromSaldo);
+            toAccount.setSaldo(toSaldo);
+            throw e;
+        }
         return events;
+    }
+
+    /**
+     * Returns the BankAccount of the given player in the given game.
+     * @param gameid Game where the BankAccount is connected to.
+     * @param playerId Player which the BankAccount is connected to.
+     * @return BankAccount you requested.
+     */
+    private BankAccount getBankAccount(int gameid, String playerId) {
+        Bank bank = bankList.getBank(gameid);
+        if (bank == null)
+            throw new BankNotFoundException();
+        BankAccount bankAccountFrom = bank.getBankAccount(playerId);
+        if (bankAccountFrom == null)
+            throw new BankAccountNotFoundException();
+        return bankAccountFrom;
     }
 }
--- src/test/java/restopoly/accesslayer/banks/BanksControllerTest.java
+++ src/test/java/restopoly/accesslayer/banks/BanksControllerTest.java
@@ -38,64 +38,115 @@ public class BanksControllerTest {
     @Test
     public void testAll() {
 
-        Player player = new Player("simon", "Simon der Grune", new Place("Wonderland"));
+        Player playerSimon = new Player("simon", "Simon der Grune", new Place("Wonderland"));
+        Player playerPatrick = new Player("patrick", "Patrick der Linke", new Place("Hyrule"));
         Game game = new Game(0);
-        BankAccount bankAccount = new BankAccount(new GamePlayer(player, game.getGameid()), 0);
+        BankAccount bankAccountSimon = new BankAccount(new GamePlayer(playerSimon, game.getGameid()), 0);
+        BankAccount bankAccountPatrick = new BankAccount(new GamePlayer(playerPatrick, game.getGameid()), 42);
 
         // POST --> Creates a bank account for one specific game / user combo.
         given()
             .contentType(ContentType.JSON)
-            .body(bankAccount)
+            .body(bankAccountSimon)
             .when().post("/banks/" + game.getGameid() + "/players").then()
             .statusCode(HttpStatus.CREATED.value());
 
         // GET --> GET one player with a banking account for one game.
         given().when()
-            .get("/banks/0/players").then()
+            .get("/banks/" + game.getGameid() + "/players").then()
             .statusCode(HttpStatus.OK.value())
-            .body("player[0].player.id", Matchers.equalTo(player.getId()))
-            .body("saldo[0]", Matchers.equalTo(bankAccount.getSaldo()));
+            .body("player[0].player.id", Matchers.equalTo(playerSimon.getId()))
+            .body("saldo[0]", Matchers.equalTo(bankAccountSimon.getSaldo()));
 
         // POST --> Can't create the same account twice.
         given()
             .contentType(ContentType.JSON)
-            .body(bankAccount)
+            .body(bankAccountSimon)
             .when().post("/banks/" + game.getGameid() + "/players").then()
             .statusCode(HttpStatus.CONFLICT.value());
 
         // POST --> Add money (2000) to my bank account (from the bank).
         given()
             .body("Schweigen der Loge der Maurer.")
-            .post("/banks/" + game.getGameid() + "/transfer/to/" + player.getId() + "/2000").then()
+            .post("/banks/" + game.getGameid() + "/transfer/to/" + playerSimon.getId() + "/2000").then()
             .statusCode(HttpStatus.CREATED.value());
 
         // GET --> GET one player with a banking account for one game.
         given().when()
-            .get("/banks/0/players").then()
+            .get("/banks/" + game.getGameid() + "/players").then()
             .statusCode(HttpStatus.OK.value())
-            .body("player[0].player.id", Matchers.equalTo(player.getId()))
+            .body("player[0].player.id", Matchers.equalTo(playerSimon.getId()))
             .body("saldo[0]", Matchers.equalTo(2000));
 
         // POST --> Add money (2000) to my bank account (from the bank).
         given()
             .body("Schmiergelder, vermoegenswirksame Leistungen.")
-            .post("/banks/" + game.getGameid() + "/transfer/from/" + player.getId() + "/1000").then()
+            .post("/banks/" + game.getGameid() + "/transfer/from/" + playerSimon.getId() + "/1000").then()
             .statusCode(HttpStatus.CREATED.value());
 
         // GET --> GET one player with a banking account for one game.
         given().when()
-            .get("/banks/0/players").then()
+            .get("/banks/" + game.getGameid() + "/players").then()
             .statusCode(HttpStatus.OK.value())
-            .body("player[0].player.id", Matchers.equalTo(player.getId()))
+            .body("player[0].player.id", Matchers.equalTo(playerSimon.getId()))
             .body("saldo[0]", Matchers.equalTo(1000));
 
         // POST --> Add money (2000) to my bank account (from the bank).
         given()
             .body("Ich kauf mir die Welt.")
-            .post("/banks/" + game.getGameid() + "/transfer/from/" + player.getId() + "/2000").then()
+            .post("/banks/" + game.getGameid() + "/transfer/from/" + playerSimon.getId() + "/2000").then()
             .statusCode(HttpStatus.FORBIDDEN.value());
 
+
+        // POST --> Creates a bank account for one specific game / user combo.
+        given()
+            .contentType(ContentType.JSON)
+            .body(bankAccountPatrick)
+            .when().post("/banks/" + game.getGameid() + "/players").then()
+            .statusCode(HttpStatus.CREATED.value());
+
         // POST --> POST money to another players banking account.
-        // TODO: The other tests for our banking service.
+        given()
+            .body("Hier armer Schlucker.")
+            .post("/banks/" + game.getGameid() + "/transfer/from/" + playerSimon.getId() + "/to/" + playerPatrick.getId() + "/100").then()
+            .statusCode(HttpStatus.CREATED.value());
+
+        // GET --> GET one player with a banking account for one game.
+        given().when()
+            .get("/banks/" + game.getGameid() + "/players").then()
+            .statusCode(HttpStatus.OK.value())
+            .body("player[0].player.id", Matchers.equalTo(playerSimon.getId()))
+            .body("saldo[0]", Matchers.equalTo(900));
+
+        // GET --> GET one player with a banking account for one game.
+        given().when()
+            .get("/banks/" + game.getGameid() + "/players").then()
+            .statusCode(HttpStatus.OK.value())
+            .body("player[1].player.id", Matchers.equalTo(playerPatrick.getId()))
+            .body("saldo[1]", Matchers.equalTo(142));
+
+        // POST --> POST money to another players banking account.
+        // This should run into an Exception and the sados should be as they were before transaction.
+        System.out.println("\nThe next RuntimeException is thrown to test transactions! Don't respect it.\n");
+        given()
+            .body("Supercalifragilisticexpialigetisch")
+            .post("/banks/" + game.getGameid() + "/transfer/from/" + playerPatrick.getId() + "/to/" + playerSimon.getId() + "/100").then()
+            .statusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+
+        // Now check both accounts if the saldo equals the saldo before the transaction.
+        // GET --> GET one player with a banking account for one game.
+        given().when()
+            .get("/banks/" + game.getGameid() + "/players").then()
+            .statusCode(HttpStatus.OK.value())
+            .body("player[0].player.id", Matchers.equalTo(playerSimon.getId()))
+            .body("saldo[0]", Matchers.equalTo(900));
+
+        // GET --> GET one player with a banking account for one game.
+        given().when()
+            .get("/banks/" + game.getGameid() + "/players").then()
+            .statusCode(HttpStatus.OK.value())
+            .body("player[1].player.id", Matchers.equalTo(playerPatrick.getId()))
+            .body("saldo[1]", Matchers.equalTo(142));
+
     }
 }
